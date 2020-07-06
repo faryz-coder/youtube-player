24d11a0eab2a844496f60e9385f37856725e65ab
Kaitou Ryz <faryz.coder@gmail.com>
2020-07-07 05:51:25 +0800

    youtube player in apps

---
+++ app/src/main/java/com/example/youtubeplayerapi/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.youtubeplayerapi
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val youTubePlayerView = yVideoPlayer
+        lifecycle.addObserver(youTubePlayerView)
+
+        youTubePlayerView.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+            override fun onReady(youTubePlayer: YouTubePlayer) {
+                val videoId = "tgGJQh1oY6M"
+                youTubePlayer.loadVideo(videoId, 0f)
+            }
+        })
+
+
+    }
+}
